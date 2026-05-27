57027ff7452c69b62883704ed458c2007fa87699
panchaltrilok786coder <panchaltrilok786@gmail.com>
2026-05-27 10:16:40 +0530

    Implement CORS support in verify_payment handler
    Added CORS headers to handle preflight requests and allow POST method.

--- api/verify_payment.js
+++ api/verify_payment.js
@@ -1,7 +1,34 @@
 import crypto from "crypto";
 
 export default async function handler(req, res) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
 
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type"
+  );
+
+  if (req.method === "OPTIONS") {
+
+    return res.status(200).end();
+
+  }
+
+  if (req.method !== "POST") {
+
+    return res.status(405).json({
+      success: false
+    });
+
+  }
   // only allow POST requests
   if (req.method !== "POST") {
 
